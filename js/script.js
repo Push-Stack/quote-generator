03f085d9caed5e9455146ed4ea0f39419fb930a8
Usama Bilal <usamabilal444@hotmail.com>
2021-10-20 00:15:59 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,79 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteTextContainer = document.getElementById("quote-text");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterButton = document.getElementById("twitter-btn");
+const newQuoteButton = document.getElementById("newQuote-btn");
+const loadingContainer = document.getElementById("loading-container");
+
+const apiURL =
+  "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+const proxyAPI = "https://cors-anywhere.herokuapp.com/";
+
+let errorCount = 0;
+
+function showLoadingSpinner() {
+  quoteContainer.hidden = true;
+  loadingContainer.hidden = false;
+}
+
+function removeLoadingSpinner() {
+  loadingContainer.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+function setQuote(quote) {
+  if (quote.length > 120) {
+    quoteTextContainer.classList.add("long-quote");
+  } else {
+    quoteTextContainer.classList.remove("long-quote");
+  }
+  quoteText.innerText = quote;
+}
+
+function setAuthor(author) {
+  if (!author) {
+    authorText.innerText = "Unknown";
+  } else {
+    authorText.innerText = author;
+  }
+}
+
+async function getQuote() {
+  try {
+    showLoadingSpinner();
+
+    const response = await fetch(proxyAPI + apiURL);
+    const data = await response.json();
+
+    setAuthor(data.quoteAuthor);
+
+    setQuote(data.quoteText);
+
+    errorCount = 0;
+
+    removeLoadingSpinner();
+  } catch (error) {
+    if (errorCount <= 5) {
+      errorCount++;
+      getQuote();
+    } else {
+      removeLoadingSpinner();
+
+      setQuote("An error occured while fetching Quotes from an API");
+      setAuthor(null);
+      quoteText.style.color = "red";
+    }
+  }
+}
+
+function shareQuoteOnTwitter() {
+  const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+
+  window.open(twitterURL, "_blank");
+}
+
+newQuoteButton.addEventListener("click", getQuote);
+twitterButton.addEventListener("click", shareQuoteOnTwitter);
+
+getQuote();
